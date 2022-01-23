0245e3784df011fb187dba0a2ed7496d3ba2aa02
dylan-mp4 <72610316+dylan-mp4@users.noreply.github.com>
2022-01-23 08:04:19 +0000

    finally fixed footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,35 +1,35 @@
-<footer class="fixed bottom-0 w-full py-5 mt-5">
+<footer class="bottom-0 w-full py-5 mt-5">
     <div class="w-4/5 pb-3 m-auto border-b-2 ">
         <div class="flow-root">
             <p class="float-left py-4 sm:text-s pt-2 text-main">
-                <span class="footer-nav pb-1 mr-5">
+                <span class=" pb-1 mr-5">
                     <a href="/">
                         HOME
                     </a>
                 </span>
-                <span class="footer-nav pb-1 mr-5">
+                <span class="pb-1 mr-5">
                     <a href="/blog">
                         BLOG
                     </a>
                 </span>
-                <span class="footer-nav pb-1 mr-5">
+                <span class="pb-1 mr-5">
                     <a href="/login">
                         LOGIN
                     </a>
                 </span>
-                <span class="footer-nav pb-1 mr-5">
+                <span class="pb-1 mr-5">
                     <a href="/register">
                         REGISTER
                     </a>
                 </span>
-                <span class="footer-nav pb-1 mr-5">
+                <span class="pb-1 mr-5">
                     <a href="https://game.immature.live">
                         GAME
                     </a>
                 </span>
 
             </p>
-            <p class="footer-nav float-right pb-3 text-s ml-10 text-main pt-2">
+            <p class="float-right pb-3 text-s ml-10 text-main pt-2">
                 © Copyright 2021 Team 26
             </p>
         </div>
